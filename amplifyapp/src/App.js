40c2cdecf952487550b12f796a3662e66c1b1854
SergiodelaMata <sergio.matam@edu.uah.es>
2020-06-14 11:56:22 +0200

    Minor change.

--- amplifyapp/src/App.js
+++ amplifyapp/src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Práctica 3 Sergio de la Mata Moratilla.
         </p>
         <a
           className="App-link"
